dcc16403b21740eb6b168d25d15fc40b0a7c2826
Prof. Eric Anderson <anderse4@ucmail.uc.edu>
2023-12-04 12:56:53 -0500

    Made JS external and reformatted content

---
+++ random-info.js
@@ -0,0 +1,17 @@
+// The list of possible choices
+function displayRandomInfo() {
+   var infoList = [
+      "Ivan",
+      "Cookie",
+      "Gary",
+      "Vladimir",
+      "Tortilla",
+      "<h2>I'm an h2!</h2>"
+   ];
+
+   // Generate a random number and pick that item from the array
+   var randomIndex = Math.floor(Math.random() * infoList.length);
+   
+   // Write that information in the HTML element that has the ID of "info"
+   document.getElementById("info").innerHTML = infoList[randomIndex];
+}
