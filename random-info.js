f07d06a677c6956f8cb6b9394e0ec7b0e842d740
PhelpsKP <phelpskp@mail.uc.edu>
2024-03-22 11:42:31 -0400

    Revert "Created dynamic button with jS"
    This reverts commit dd792d5fb55903bceac46f63489a3f7cfc5e76ea.

--- random-info.js
+++ random-info.js
@@ -1,19 +1,9 @@
 // List of possible choices
-function showInfo() {
-    var infoList = [
-        "Bork",
-        "Bark",
-        "Some other dog idk I only have two",
-        "Freya is a cat",
-        "Meow",
-        "Dude Bark just ate my hat and I'm VERY mad at him right now",
-        "<h2> You'll have to forgive me. Kentucky just lost in the first round so I'm not very happy with cats right now  </h2>"
-    ];
+
 
    // Generate a random number and pick that item from the array
-    var randomizer = Math.floor(Math.random() * infoList.length);
+
    
    // Write that information in the HTML element that has the ID of "info"
-    document.getElementById("info").innerHTML = infoList[randomizer];
+
    
-   };
