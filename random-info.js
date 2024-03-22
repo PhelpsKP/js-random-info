5b668b46afd0af7a3f1e528a5720b779004dcd97
PhelpsKP <phelpskp@mail.uc.edu>
2024-03-22 11:59:49 -0400

    Added jS button

--- random-info.js
+++ random-info.js
@@ -1,9 +1,18 @@
 // List of possible choices
-
+function showInfo() {
+    var infoList = [
+        "Bark",
+        "Bork",
+        "Idk I only have two dogs",
+        "Freya is a cat",
+        "Actually I don't like cats right now. UK pissed me off losing to Oakland in the tournament",
+        "Hopefully the Bengals make the playoffs this year",
+        "<h2>Kitty goes meow</h2>",
+    ];
 
    // Generate a random number and pick that item from the array
-
+var randomizer = Math.floor(Math.random() * infoList.length);
    
    // Write that information in the HTML element that has the ID of "info"
-
-   
+document.getElementById("info").innerHTML = infoList[randomizer];
+};
